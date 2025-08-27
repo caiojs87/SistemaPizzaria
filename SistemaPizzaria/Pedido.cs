f61d19760ecb5c6afe5528229e4b0b33578beb09
Caio <caiods786@gmail.com>
2025-08-27 17:41:39 -0300

    Adicionando os elementos no form pedido

--- SistemaPizzaria/Pedido.cs
+++ SistemaPizzaria/Pedido.cs
@@ -16,5 +16,10 @@ namespace SistemaPizzaria
         {
             InitializeComponent();
         }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
